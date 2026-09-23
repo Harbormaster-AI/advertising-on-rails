ef95627c30e30b389fcaa14052e7135df513298b
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-23 09:01:55 -0400

    Initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
       CreatedBy = "Harbormaster"
       Blueprint = "RubyonRails"
       DomainModel = "Advertising Industry Domain Model"
-      CertificationId = "5314b79f-f743-4f28-84f0-339cc05e3f37"
+      CertificationId = "c9cbdc80-3a4f-4f52-b0be-c0b78f0a8eb0"
     }
   }
 }
@@ -39,7 +39,7 @@ resource "local_file" "private_key_pem" {
 }
 
 resource "aws_key_pair" "generated" {
-  key_name   = "pjsk-sshtest-0.16227102906458235"
+  key_name   = "pjsk-sshtest-0.43990079623210077"
   public_key = tls_private_key.generated.public_key_openssh
 
   lifecycle {
